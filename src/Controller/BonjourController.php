0c7054d6d6d7560d64710f417db47daa1e959b3e
Tyranikwerewolf <83700003+Gaeram@users.noreply.github.com>
2022-06-30 16:56:05 +0200

    commenting unused use class

--- src/Controller/BonjourController.php
+++ src/Controller/BonjourController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller;
 
-use phpDocumentor\Reflection\Types\This;
+// use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
